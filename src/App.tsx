de0b72fdf56832f94d22f7c824e14c78985ad997
oalgammal <oalgammal@gmail.com>
2024-06-10 12:01:13 +0300

    using --template typescrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+
 import './App.css';
 import {
   QueryClientProvider,
 } from '@tanstack/react-query'
-import React from 'react';
-import DeleteUser from './components/DeleteUser';
 import { queryClient } from './helpers/query-client';
 import AppWrapper from './components/AppWrapper';
 
